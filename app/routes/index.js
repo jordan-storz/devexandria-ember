3a33d311f48bea7bb6cc49dec7e778fb5a5f0fdb
jordan-storz <jordan.storz.1@gmail.com>
2017-01-21 21:22:55 -0700

    index route redirects to bookmarks route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return Ember.RSVP.hash({
-      tags: this.get('store').findAll('tag')
-    });
+  beforeModel() {
+    this.transitionTo('bookmarks');
   }
 });
